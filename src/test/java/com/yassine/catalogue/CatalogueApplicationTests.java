1f23d77caf8c0586df3e70eaf1d72c165391a839
Yassinean <yassinedubraska@gmail.com>
2024-12-02 14:50:17 +0100

    Setuping the project

---
+++ src/test/java/com/yassine/catalogue/CatalogueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yassine.catalogue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
