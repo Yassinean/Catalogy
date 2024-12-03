f971c01552c5a693a32063b77ead0d1b138cd24a
Yassinean <yassinedubraska@gmail.com>
2024-12-03 13:03:37 +0100

    Implementing entities with their relationship

--- src/main/java/com/yassine/catalogue/entities/Category.java
+++ src/main/java/com/yassine/catalogue/entities/Category.java
@@ -1,4 +1,22 @@
 package com.yassine.catalogue.entities;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table(name = "categories")
+@Data
+@RequiredArgsConstructor
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nom;
+    private String description;
+
+    @OneToMany(mappedBy = "categorie", cascade = CascadeType.ALL)
+    private List<Product> produits;
 }
